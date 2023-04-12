d0881017e9361d85c6f74d0e53ea926d87a3b1fe
Pier Luc Ducharme <plducharme@cegepsth.qc.ca>
2023-04-12 09:45:10 -0400

    fixer typos cours 11

--- cours11/citations.py
+++ cours11/citations.py
@@ -1,13 +1,13 @@
 # Lire le fichier ligne par ligne
 # Creer une classe Citation pour representer une citation (citation, auteur)
-# Pour chaque ligne, creer un objet Citation
-# Creer une methode statique qui va retourner une citation au hasard a partir d'une liste d'objets
+# Pour chaque ligne dans citations.txt, creer un objet Citation
+# Creer une methode statique qui va retourner une citation au hasard a partir d'une liste d'objets Citation
 # Creer une methode statique pour trier les citations par ordre alphabetique
 #   (sans utiliser sort, votre choix d'algo)
 # Creer une methode statique pour trier les citations par ordre alphabetique de l'auteur
 #   (sans utiliser sort, votre choix d'algo)
-# Creer une methode pour compresser chaque citation dans un fichier texte dans le repertoire "tmp"
+# Creer une methode pour ecrire chaque citation dans un fichier texte dans le repertoire "tmp"
 #   Le nom de fichier doit etre le nom de l'auteur, si existant, ajoute la citation au fichier
-# Creer une methode statique pour lire tous les fichiers du repertoire et
+# Creer une methode statique pour lire tous les fichiers du repertoire tmp et
 #   recreer le fichier original sous le nom 'citations_nouveau.txt'
 # Comparer les fichiers
