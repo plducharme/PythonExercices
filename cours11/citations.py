07b846dfe4b2ed16f6e21a85bd967e6ab8009cdd
Pier Luc Ducharme <plducharme@cegepsth.qc.ca>
2023-04-12 09:28:20 -0400

    fixer typos cours 11

--- cours11/citations.py
+++ cours11/citations.py
@@ -3,9 +3,9 @@
 # Pour chaque ligne, creer un objet Citation
 # Creer une methode statique qui va retourner une citation au hasard a partir d'une liste d'objets
 # Creer une methode statique pour trier les citations par ordre alphabetique
-#   (sans utilisert sort, votre choix d'algo)
+#   (sans utiliser sort, votre choix d'algo)
 # Creer une methode statique pour trier les citations par ordre alphabetique de l'auteur
-#   (sans utilisert sort, votre choix d'algo)
+#   (sans utiliser sort, votre choix d'algo)
 # Creer une methode pour compresser chaque citation dans un fichier texte dans le repertoire "tmp"
 #   Le nom de fichier doit etre le nom de l'auteur, si existant, ajoute la citation au fichier
 # Creer une methode statique pour lire tous les fichiers du repertoire et
